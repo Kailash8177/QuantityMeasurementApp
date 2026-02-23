6cf417bd23d45eeb766013a60580b4aed19485f9
Kailash Nath Yadav <your_email@example.com>
2026-02-23 16:48:06 +0530

    Kailash Nath Yadav]. Add Implemented UC1

---
+++ QuantityMeasurementApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuantityMeasurementApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first value in feet:");
+            double value1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second value in feet:");
+            double value2 = Convert.ToDouble(Console.ReadLine());
+
+            Feet feet1 = new Feet(value1);
+            Feet feet2 = new Feet(value2);
+
+            bool result = feet1.Equals(feet2);
+
+            Console.WriteLine("Equal: " + result);
+        }
+    }
+}
