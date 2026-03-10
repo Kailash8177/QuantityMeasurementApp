89de91fb524f93b5e068514937ec7a99631aaaf4
Kailash Nath Yadav <your_email@example.com>
2026-03-10 16:44:37 +0530

    [Kailash Nath Yadav] Added feature/UC11-VolumeMeasurementEquality/QuantityMeasurementApp

---
+++ QuantityMeasurementApp.Core/Models/Quantity.cs
@@ -0,0 +1,130 @@
+using System;
+using QuantityMeasurementApp.Core.Units;
+
+namespace QuantityMeasurementApp.Core.Models
+{
+    public class Quantity<U>
+    {
+        public double Value { get; }
+        public U Unit { get; }
+
+        public Quantity(double value, U unit)
+        {
+            if (unit == null)
+                throw new ArgumentException("Unit cannot be null");
+
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException("Invalid value");
+
+            Value = value;
+            Unit = unit;
+        }
+
+        public Quantity<U> ConvertTo(U targetUnit)
+        {
+            double baseValue;
+
+            if (Unit is LengthUnit lu)
+                baseValue = lu.ConvertToBaseUnit(Value);
+            else if (Unit is WeightUnit wu)
+                baseValue = wu.ConvertToBaseUnit(Value);
+            else if (Unit is VolumeUnit vu)
+                baseValue = vu.ConvertToBaseUnit(Value);
+            else
+                throw new Exception("Unsupported unit");
+
+            double converted;
+
+            if (targetUnit is LengthUnit tlu)
+                converted = tlu.ConvertFromBaseUnit(baseValue);
+            else if (targetUnit is WeightUnit twu)
+                converted = twu.ConvertFromBaseUnit(baseValue);
+            else if (targetUnit is VolumeUnit tvu)
+                converted = tvu.ConvertFromBaseUnit(baseValue);
+            else
+                throw new Exception("Unsupported unit");
+
+            return new Quantity<U>(Math.Round(converted, 5), targetUnit);
+        }
+
+        public Quantity<U> Add(Quantity<U> other, U targetUnit)
+        {
+            double base1;
+            double base2;
+
+            if (Unit is LengthUnit lu)
+                base1 = lu.ConvertToBaseUnit(Value);
+            else if (Unit is WeightUnit wu)
+                base1 = wu.ConvertToBaseUnit(Value);
+            else if (Unit is VolumeUnit vu)
+                base1 = vu.ConvertToBaseUnit(Value);
+            else
+                throw new Exception("Unsupported unit");
+
+            if (other.Unit is LengthUnit olu)
+                base2 = olu.ConvertToBaseUnit(other.Value);
+            else if (other.Unit is WeightUnit owu)
+                base2 = owu.ConvertToBaseUnit(other.Value);
+            else if (other.Unit is VolumeUnit ovu)
+                base2 = ovu.ConvertToBaseUnit(other.Value);
+            else
+                throw new Exception("Unsupported unit");
+
+            double sumBase = base1 + base2;
+
+            double result;
+
+            if (targetUnit is LengthUnit tlu)
+                result = tlu.ConvertFromBaseUnit(sumBase);
+            else if (targetUnit is WeightUnit twu)
+                result = twu.ConvertFromBaseUnit(sumBase);
+            else if (targetUnit is VolumeUnit tvu)
+                result = tvu.ConvertFromBaseUnit(sumBase);
+            else
+                throw new Exception("Unsupported unit");
+
+            return new Quantity<U>(Math.Round(result, 5), targetUnit);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var other = (Quantity<U>)obj;
+
+            double base1;
+            double base2;
+
+            if (Unit is LengthUnit lu)
+                base1 = lu.ConvertToBaseUnit(Value);
+            else if (Unit is WeightUnit wu)
+                base1 = wu.ConvertToBaseUnit(Value);
+            else if (Unit is VolumeUnit vu)
+                base1 = vu.ConvertToBaseUnit(Value);
+            else
+                throw new Exception("Unsupported unit");
+
+            if (other.Unit is LengthUnit olu)
+                base2 = olu.ConvertToBaseUnit(other.Value);
+            else if (other.Unit is WeightUnit owu)
+                base2 = owu.ConvertToBaseUnit(other.Value);
+            else if (other.Unit is VolumeUnit ovu)
+                base2 = ovu.ConvertToBaseUnit(other.Value);
+            else
+                throw new Exception("Unsupported unit");
+
+            return Math.Abs(base1 - base2) < 0.0001;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Quantity({Value}, {Unit})";
+        }
+    }
+}
---
+++ QuantityMeasurementApp.Core/Units/VolumeUnit.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace QuantityMeasurementApp.Core.Units
+{
+    public enum VolumeUnit
+    {
+        LITRE,
+        MILLILITRE,
+        GALLON
+    }
+
+    public static class VolumeUnitExtensions
+    {
+        public static double GetConversionFactor(this VolumeUnit unit)
+        {
+            return unit switch
+            {
+                VolumeUnit.LITRE => 1.0,
+                VolumeUnit.MILLILITRE => 0.001,
+                VolumeUnit.GALLON => 3.78541,
+                _ => throw new ArgumentException("Invalid volume unit")
+            };
+        }
+
+        public static double ConvertToBaseUnit(this VolumeUnit unit, double value)
+        {
+            return value * unit.GetConversionFactor();
+        }
+
+        public static double ConvertFromBaseUnit(this VolumeUnit unit, double baseValue)
+        {
+            return baseValue / unit.GetConversionFactor();
+        }
+
+        public static string GetUnitName(this VolumeUnit unit)
+        {
+            return unit switch
+            {
+                VolumeUnit.LITRE => "Litre",
+                VolumeUnit.MILLILITRE => "Millilitre",
+                VolumeUnit.GALLON => "Gallon",
+                _ => "Unknown"
+            };
+        }
+    }
+}
