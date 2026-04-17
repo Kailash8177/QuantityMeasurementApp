9a361bab06cd326f9f60e385fd1cbf7c79a094ba
Kailash Nath Yadav <your_email@example.com>
2026-04-17 12:08:41 +0530

    deleted old folder

--- UC18/QuantityMeasurementModelLayer/Enums/Enums.cs
+++
@@ -1,8 +0,0 @@
-namespace QuantityMeasurementModelLayer.Enums
-{
-    public enum LengthUnit      { FEET, INCHES, YARDS, CENTIMETERS }
-    public enum WeightUnit      { KILOGRAM, GRAM }
-    public enum VolumeUnit      { LITRE, MILLILITRE, GALLON }
-    public enum TemperatureUnit { CELSIUS, FAHRENHEIT, KELVIN }
-    public enum ArithmeticOperation { ADD, SUBTRACT, DIVIDE }
-}
